78e0f5f554f4e5b9db698f033d16b4a364c44c0b
yokn <yokn@protonmail.com>
2021-01-15 12:02:50 +0300

    refactor past/upcoming filters to scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances, source: :user
+
+  scope :past, -> { where('date < ? ', Time.current) }
+  scope :upcoming, -> { where('date > ? ', Time.current) }
 end
