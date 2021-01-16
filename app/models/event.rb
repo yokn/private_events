e9f7fc0f705df02b28d78196e5e7c00a8af9a32c
yokn <yokn@protonmail.com>
2021-01-16 17:05:19 +0300

    add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
   validate :in_the_future
 
   def in_the_future
-    errors.add(:date, "Can't create a new event in the past!") unless date >= Time.current
+    if date
+      errors.add(:date, "Can't create a new event in the past!") unless date >= Time.current
+    end
   end
 
   belongs_to :user
