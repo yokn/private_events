f305f28a3da60974c83feccb7aef0724cbe547db
yokn <yokn@protonmail.com>
2021-01-17 21:34:30 +0300

    hero styling for signup/login pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
 
+  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   post 'attend', to: 'attendances#create'
