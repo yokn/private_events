5c769753fbc9b7f1b1dd24091c0fcf28f8e16093
yokn <yokn@protonmail.com>
2021-01-27 19:45:53 +0300

    update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_170506) do
+ActiveRecord::Schema.define(version: 2021_01_23_164812) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.text "name"
+    t.date "date"
+    t.text "location"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "private", default: 0
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "username"
@@ -18,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_12_22_170506) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,13 @@ User.create!([
              ])
 Event.create!([
                 { name: 'my new event!', date: '2021-02-06', location: 'my house', description: 'my description', user_id: 2, private: 0 },
-                { name: 'Birthday Party', date: '2021-01-29', location: "Bob's house", description: 'Bring gifts!', user_id: 1, private: 0 },
+                { name: 'Birthday Party', date: '2021-03-29', location: "Bob's house", description: 'Bring gifts!', user_id: 1, private: 0 },
                 { name: 'test', date: '2021-12-29', location: '123a', description: '1', user_id: 1, private: 0 },
-                { name: '17', date: '2021-01-27', location: '17', description: '17', user_id: 2, private: 0 },
+                { name: '17', date: '2021-03-27', location: '17', description: '17', user_id: 2, private: 0 },
                 { name: 'Mars Oasis', date: '2026-03-14', location: 'Mars', description: 'A slightly bigger step.', user_id: 1, private: 0 },
                 { name: 'Comet Hale–Bopp', date: '4385-01-01', location: 'Human Memorial Hall', description: 'Comet passing near TERRA. Audience required. Please comply.', user_id: 1, private: 0 },
-                { name: 'Private event', date: '2021-01-30', location: 'localhost:3000', description: 'You may only attend this event if you were invited.', user_id: 1, private: 1 },
-                { name: 'IT Meeting', date: '2021-01-25', location: 'Meeting room 2B', description: 'We will discuss what we did last week and set our targets for this week.', user_id: 1, private: 1 }
+                { name: 'Private event', date: '2021-02-28', location: 'localhost:3000', description: 'You may only attend this event if you were invited.', user_id: 1, private: 1 },
+                { name: 'IT Meeting', date: '2021-02-25', location: 'Meeting room 2B', description: 'We will discuss what we did last week and set our targets for this week.', user_id: 1, private: 1 }
               ])
 Attendance.create!([
                      { user_id: 2, event_id: 2 },
